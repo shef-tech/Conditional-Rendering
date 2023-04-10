cceccf991c922b0ee1d3e9d04ae5935508e46c77
Shefali Mandal <shefalimandal1999@gmail.com>
2023-04-10 13:53:58 +0545

    Conditioning Rendering

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Login from "./Login";
+
+var isLoggedIn = true;
+
+function App() {
+    return (
+        <div className="container">
+            {
+                isLoggedIn ? <h1>Hello</h1> : <Login />
+                /* isLoggedIn && <h1>Hello</h1> */
+            }
+        </div>
+    );
+}
+
+export default App;
